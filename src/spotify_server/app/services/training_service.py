84135a019bf909e25b12160e4d0b7ac14ab0055c
Belinus-code <79333714+Belinus-code@users.noreply.github.com>
2026-03-12 10:56:49 +0100

    test

--- src/spotify_server/app/services/training_service.py
+++ src/spotify_server/app/services/training_service.py
@@ -283,6 +283,7 @@ class TrainingService:
 
         # Alles in Klammern entfernen
         title = re.sub(r"\(.*?\)", "", title)
+        title = re.sub(r"\[.*?\]", "", title)
         # Alles hinter einem Bindestrich entfernen
         title = title.split("-")[0]
         # Whitespace bereinigen
